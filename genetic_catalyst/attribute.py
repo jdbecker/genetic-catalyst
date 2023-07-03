c7cd2efddf28ad6c92c482be35b68870b1bab0bc
jdbecker <jdbecker@users.noreply.github.com>
2023-07-03 16:32:01 +0000

    Fill out attribute list

--- genetic_catalyst/attribute.py
+++ genetic_catalyst/attribute.py
@@ -1,4 +1,13 @@
 from enum import Enum
 
 class Attribute(Enum):
+    """These are the names for the different types of Attributes a Creature can 
+    have"""
     HARVESTING = "Harvesting"
+    ABSORPTION = "Absorption"
+    STRENGTH = "Strength"
+    INTELLIGENCE = "Intelligence"
+    CUNNING = "Cunning"
+    HEALTH = "Health"
+    PERCEPTION = "Perception"
+    BANDWIDTH = "Bandwidth"
