e8d94bdb6c49a5a7b564ea19d2add145f544e3c1
jdbecker <jdbecker@users.noreply.github.com>
2023-07-06 17:06:36 +0000

    Add simple string method to Attribute and index

--- genetic_catalyst/attribute.py
+++ genetic_catalyst/attribute.py
@@ -5,14 +5,17 @@ class Attribute(Enum):
     """These are the names for the different types of Attributes a Creature can
     have"""
 
-    HEALTH = "Health"
-    STRENGTH = "Strength"
-    INTELLIGENCE = "Intelligence"
-    CUNNING = "Cunning"
-    BANDWIDTH = "Bandwidth"
-    PERCEPTION = "Perception"
-    HARVESTING = "Harvesting"
-    ABSORPTION = "Absorption"
+    HEALTH = 0
+    STRENGTH = 1
+    INTELLIGENCE = 2
+    CUNNING = 3
+    BANDWIDTH = 4
+    PERCEPTION = 5
+    HARVESTING = 6
+    ABSORPTION = 7
 
     def __repr__(self) -> str:
         return f"Attribute.{self.name}"
+
+    def __str__(self) -> str:
+        return self.name[0] + self.name[1:].lower()
